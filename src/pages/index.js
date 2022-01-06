3d0269fd75d85e1c8ce8b4c5c7cf9ee595b447a3
Hyunmin Jung <jung@hyunmin.dev>
2022-01-06 10:50:57 +0900

    feat: 학습 파일 정리

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import {useEffect} from 'react';
+
+function sleep(ms) {
+  const until = Date.now() + ms;
+  let current = Date.now();
+  while (current < until) {
+    current = Date.now();
+  }
+}
+
+export default function Home() {
+  useEffect(() => {
+    // if (typeof window !== 'undefined') {
+    window.addEventListener('load', () => {
+      console.log('window load');
+    });
+    document.addEventListener('DOMContentLoaded', () => {
+      console.log('document DOMContentLoaded');
+    });
+    // }
+  }, []);
+  sleep(5000);
+  return (
+      <button
+          type="button"
+          onClick={() => console.log('click')}>
+        button
+      </button>
+  );
+}
